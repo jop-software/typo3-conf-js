a5a97bb2baf92a94e20f15be1b7e661bf5a77f1a
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-10-11 22:11:14 +0200

    release: version 2.0.2 - set proper typo3 version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jop-software Inh. Johannes Przymusinski',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.99',
+            'typo3' => '12.0.0-12.0.99',
             'php' => '8.1.99',
         ],
         'conflicts' => [
